dfe656b14a51c044766465b0ed30a067de79e644
Juan Frauca <juancore97@gmail.com>
2026-03-03 12:13:39 +0100

    initial commit

---
+++ src/main/java/com/erp/erp_layout/ErpLayoutApplication.java
@@ -0,0 +1,13 @@
+package com.erp.erp_layout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpLayoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpLayoutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erp/erp_layout/ErpLayoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erp.erp_layout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpLayoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
